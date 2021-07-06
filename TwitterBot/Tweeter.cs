fe930bf88c28ee2284aa75e67b41ffd29c77db15
FluerasGabriel <77633365+FluerasGabriel@users.noreply.github.com>
2021-07-06 18:39:56 +0300

    Add files via upload

---
+++ TwitterBot/Tweeter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterBot
+{
+    class Tweeter
+    {
+       
+            private string oauth_consumer_key = "DfRjKDdx9vWF5khhKsZLigUvi";
+            private string oauth_consumer_secret = "XPc7MZ3B2H4HwvEIoL52kybwFKcnlGlQQh1CgpTbleSIipjk7v";
+            private string oauth_token = "1381538964587286528-ZmrZDsg33ixTlUgdIAS4EF9Yj3vri3";
+            private string oauth_token_secret = "p91qMABE5v3D5irdQoMCTfOnTRMWBfPju4Y3LUqwI5jNS";
+
+            public string Oauth_consumer_key { get { return this.oauth_consumer_key; } }
+            public string Oauth_consumer_secret { get { return this.oauth_consumer_secret; } }
+            public string Oauth_token { get { return this.oauth_token; } }
+            public string Oauth_token_secret { get { return this.oauth_token_secret; } }
+        
+
+    }
+}
---
+++ TwitterBot/Tweets.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using System.Windows.Forms;
+using Tweetinvi.Parameters;
+using System.IO;
+using System.Threading;
+
+namespace TwitterBot
+{
+    class Tweets
+    {
+
+        public async void AddTweet(string tweet, TwitterClient client)
+        {
+            try
+            {
+                await client.Tweets.PublishTweetAsync(tweet);
+                MessageBox.Show("You're tweet have been posted", "Tweet Request.");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public async void AddTweetWithImg(string tweet, string img, TwitterClient client)
+        {
+            try
+            {
+                var imgLocation = File.ReadAllBytes(img);
+                var uploadImg = await client.Upload.UploadTweetImageAsync(imgLocation);
+                var tweetWithImg = await client.Tweets.PublishTweetAsync(new PublishTweetParameters(tweet)
+                {
+                    Medias = { uploadImg }
+                });
+                MessageBox.Show("You're tweet have been posted", "Tweet Request.");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+       
+        }
+        public void FileTweetWithImg(int time,string location ,TwitterClient client)
+        {
+            try
+            {
+                var file = File.ReadAllLines(location);
+                for(int i = 0; i <= file.Length -1; i++)
+                {
+                    var tweet = file[i];
+                    var image = file[i + 1];
+                    AddTweetWithImg(tweet, image, client);
+                    Thread.Sleep(time);
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public void TweetReadFromFile(int time,string location,TwitterClient client)
+        {
+            try
+            {
+                var file = File.ReadAllLines(location);
+                for (int i = 0; i <= file.Length - 1; i++)
+                {
+                    AddTweet(file[i], client);
+                    Thread.Sleep(time);
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+          
+        }
+       
+        
+        public async void AddFriend(string name, TwitterClient client)
+        {
+            try
+            {
+                await client.Users.FollowUserAsync(name);
+                MessageBox.Show("You followed " + name, "Follow Request.");
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public async void UserSettings(string ProfileImg, string ProfileBanner, TwitterClient client)
+        {
+                    
+            if (!String.IsNullOrEmpty(ProfileImg)) {
+                try
+                {
+                    var ProfileImgLocation = File.ReadAllBytes(ProfileImg);
+                    await  client.AccountSettings.UpdateProfileImageAsync(ProfileImgLocation);
+                    MessageBox.Show("Profile Picture have been updated");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                }
+            if (!String.IsNullOrEmpty(ProfileBanner)) {
+                try
+                {
+                    var BannerImgLocation = File.ReadAllBytes(ProfileBanner);
+                    await  client.AccountSettings.UpdateProfileImageAsync(BannerImgLocation);
+                    MessageBox.Show("Profile Banner have been updated");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                }
+        }
+    }
+}
